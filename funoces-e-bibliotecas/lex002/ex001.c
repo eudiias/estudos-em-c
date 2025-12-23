7b328b12650250d784c406383582f39708bf5093
Carlos-Diasz <carlos.dias.o.n@gmail.com>
2025-12-23 17:47:36 -0300

    Atividade

---
+++ funoces-e-bibliotecas/lex002/ex001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int base = 2, expoente = 3;
+
+    printf("2 elevado a 3 e: %.2f\n", pow(base, expoente));
+    printf("Raiz quadrada de 25: %.2f\n", sqrt(25));
+
+    return 0;
+}
