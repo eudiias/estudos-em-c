ef555abfda918a244016730b808482686cb62ed6
eudiias <carlos.dias.o.n@gmail.com>
2026-01-12 23:37:51 -0300

    Revisao de matrizes

---
+++ estrutura-de-dados-basica/lex002/ex001.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int notas[3][2];
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            printf("Digite a nota %d do aluno %d: ", j + 1, i + 1);
+            scanf("%d", &notas[i][j]);
+        }
+    }
+
+    printf("\n==== NOTAS ====\n");
+
+    for(int i = 0; i < 3; i++)
+    {
+        printf("ALUNO %d: \n", i + 1);
+        for(int j = 0; j < 2; j++)
+        {
+            printf("NOTA %d: %d\n", j + 1, notas[i][j]);
+        }
+    }
+
+    return 0;
+}
